73fb24e919ccaaf9c86fdbee34f9965978acb413
Othniel Powell <othniel@live.ca>
2025-11-13 13:12:02 -0500

    Update ProductMap.java

--- src/assignment3/ProductMap.java
+++ src/assignment3/ProductMap.java
@@ -1,1 +1,26 @@
+import java.util.HashMap;
+
+public class ProductMap {
+    private HashMap<Integer, Integer> products;
+
+    public ProductMap() {
+        products = new HashMap<>();
+    }
+
+    public void updateProduct(int productId, int itemsPurchased) {
+        int currentCount = products.getOrDefault(productId, 0);
+        products.put(productId, currentCount + itemsPurchased);
+    }
+
+    public HashMap<Integer, Integer> getProducts() {
+        return products;
+    }
+
+    public void printProducts() {
+        System.out.println("\n=== Products Summary ===");
+        for (Integer id : products.keySet()) {
+            System.out.println("Product ID: " + id + " | Total Purchased: " + products.get(id));
+        }
+    }
+}
 
