2dbab8baedc04d57e9cc82208a91a8b28077c0eb
Dustin <dustingreville@gmail.com>
2025-11-14 16:10:13 -0500

    Added package declarations

--- src/assignment3/Customer.java
+++ src/assignment3/Customer.java
@@ -1,3 +1,5 @@
+package assignment3;
+
 public class Customer {
     private String firstName;
     private String lastName;
--- src/assignment3/ProductMap.java
+++ src/assignment3/ProductMap.java
@@ -1,3 +1,5 @@
+package assignment3;
+
 import java.util.HashMap;
 
 public class ProductMap {
